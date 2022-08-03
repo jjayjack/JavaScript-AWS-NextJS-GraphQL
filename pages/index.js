d7bf1bc9f24b3568aa2b51eb2d29dd914510d6cc
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-08-03 13:38:21 -0500

    Testing Tailwind CSS

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<h1 className="text-3xl font-bold bg-stone-500 underline text-teal-100 decoration-teal-100">
+			Hello world!
+		</h1>
+	);
 }
