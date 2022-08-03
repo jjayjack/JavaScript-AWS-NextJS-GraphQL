693d310f6151b0b462e6127125bbeb9edb0e5d67
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-08-03 17:22:57 -0500

    Added Format & Sign out feature

--- pages/profile.js
+++ pages/profile.js
@@ -16,8 +16,13 @@ function Profile() {
 	return (
 		<div>
 			<h1 className="text-3xl font-semibold tracking-wide mt-6">Profile</h1>
-			<h1 className="font-medium text-gray-500 my-2">{user.username}</h1>
-			<p className="text-small text-gray-500 mb-6">{user.attributes.email}</p>
+			<h1 className="font-medium text-gray-500 my-2">
+				Username: {user.username}
+			</h1>
+			<p className="text-small text-gray-500 mb-6">
+				Email: {user.attributes.email}
+			</p>
+			<AmplifySignOut />
 		</div>
 	);
 }
