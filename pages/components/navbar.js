6776e735a4ffc9737888b5efc0b77e43873415bd
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-08-03 16:00:40 -0500

    Adding Navigation w/ state check

---
+++ pages/components/navbar.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+
+import "../../configureAmplify";
+
+const Navbar = () => {
+	const [signedUser, setSignedUser] = useState[false];
+};
