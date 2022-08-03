2da4528457f6bbc2fd9f46805c0d64c4ceb974f8
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-08-03 16:02:13 -0500

    Rendering return for component

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -5,4 +5,20 @@ import "../../configureAmplify";
 
 const Navbar = () => {
 	const [signedUser, setSignedUser] = useState[false];
+
+	return (
+		<nav className="flex justify-center pt-3 pb-3 space-x-4 border-b bg-cyan-500 border-gray-300">
+			{[
+				["Home", "/"],
+				["Create Post", "/create-post"],
+				["Profile", "/profile"]
+			].map(([title, url], index) => (
+				<Link key={index} href={url}>
+					<a className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate hover:text-slate-900">
+						{title}
+					</a>
+				</Link>
+			))}
+		</nav>
+	);
 };
