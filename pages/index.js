0ca028c928e01e99351df880f68adfd8e42d9864
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-08-03 15:19:49 -0500

    Connecting Database stored data to application

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
+import { useState, useEffect } from "react";
+import { API } from "aws-amplify";
+import { listPosts } from "../src/graphql/queries";
+
 export default function Home() {
+	const [posts, setPosts] = useState([]);
+
+	async function fetchPosts() {
+		const postData = await API.graphql({
+			query: listPosts
+		});
+		setPosts(postData.data.listPosts.items);
+	}
+
 	return (
 		<h1 className="text-3xl font-bold bg-stone-500 underline text-teal-100 decoration-teal-100">
 			Hello world!
