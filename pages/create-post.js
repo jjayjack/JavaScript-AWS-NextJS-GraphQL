8f3402ab39bdca0d1be4387c983cb784c10dcf6a
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-08-03 18:40:44 -0500

    Setup of Auth for creating post

--- pages/create-post.js
+++ pages/create-post.js
@@ -1,6 +1,9 @@
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { useState, useRef, React } from "react";
 import { API } from "aws-amplify";
+import { useRouter } from "next/router";
+import { v4 as uuid } from "uuid";
+import { createPost } from "../src/graphql/mutations";
 
 const initialState = { title: "", content: "" };
 
@@ -15,6 +18,26 @@ function CreatePost() {
 			[event.target.name]: event.target.value
 		}));
 	}
+
+	async function createNewPost() {
+		if (!title || !content) return;
+
+		const id = uuid();
+		post.id = id;
+
+		await API.graphql({
+			query: createPost,
+			variables: { input: post },
+			authMode: "AMAZON_COGNITO_USER_POOLS"
+		});
+
+		// router.push(`/post/${id}`)
+	}
+	return (
+		<div>
+			<h1>Create New Post</h1>
+		</div>
+	);
 }
 
 export default CreatePost;
