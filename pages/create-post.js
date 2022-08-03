b535c4fb1645788f8e940ff9f89acd96ba65c303
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-08-03 18:29:08 -0500

    Initialize changing for create post function

--- pages/create-post.js
+++ pages/create-post.js
@@ -1,3 +1,20 @@
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { useState, useRef, React } from "react";
 import { API } from "aws-amplify";
+
+const initialState = { title: "", content: "" };
+
+function CreatePost() {
+	const [post, setPost] = useState(initialState);
+	const { title, content } = post;
+	const router = useRouter();
+
+	function onChange(event) {
+		setPost(() => ({
+			...post,
+			[event.target.name]: event.target.value
+		}));
+	}
+}
+
+export default CreatePost;
