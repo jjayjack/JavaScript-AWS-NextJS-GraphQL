31331e8894f9371ae3d292fc78758174a612d995
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-08-05 11:01:52 -0500

    CSS formatting and layout changes

--- pages/my-posts.js
+++ pages/my-posts.js
@@ -23,22 +23,35 @@ export default function MyPosts() {
 
 	return (
 		<div className="container lowercase rounded border-2 border-quaternary m-10 p-5 bg-quaternary">
-			<h1 className="rounded pb-3 text-tertiary text-5xl font-semibold tracking-wide ">
+			<h1 className="rounded pb-3 text-tertiary text-5xl font-semibold tracking-wide">
 				My Posts
 			</h1>
-			<ul className="container cursor-pointer rounded bg-tertiary mt-5 p-3 text-primary">
+			<ul className="flex flex-wrap container rounded bg-tertiary mt-5 p-3 text-primary text-center">
 				{posts.map((post, index) => (
-					<Link key={index} href={`/posts/${post.id}`}>
-						<li className="rounded-lg pl-2 text-justify hover:indent-4 hover:text-tertiary hover:bg-quaternary">
-							<h2 className="text-xl font-bold">{post.title}</h2>
-							<p className="mt-2">
-								created on:{" "}
-								{DateTime.fromISO(post.createdAt).toFormat(
-									"dd LLL yyyy @ HH:mm ZZZZ"
-								)}
-							</p>
-						</li>
-					</Link>
+					<li key={index} className="rounded-lg pl-2 text-justify p-2 m-3">
+						<h2 className="text-xl font-bold">{post.title}</h2>
+						<p className="m-2">
+							created:{" "}
+							{DateTime.fromISO(post.createdAt).toFormat(
+								"dd LLL yyyy @ HH:mm ZZZZ"
+							)}
+						</p>
+						<div className="flex container space-x-3 flex-auto">
+							<button className="rounded flex-auto bg-tertiary cursor-pointer p-2 border-2 border-secondary hover:bg-secondary hover:text-tertiary hover:font-bold">
+								<Link key={index} href={`/edit-post/${post.id}`}>
+									edit
+								</Link>
+							</button>
+							<button className="rounded flex-auto bg-tertiary cursor-pointer p-2 border-2 border-secondary hover:bg-secondary hover:text-tertiary hover:font-bold">
+								<Link key={index} href={`/posts/${post.id}`}>
+									view
+								</Link>
+							</button>
+							<button className="rounded flex-auto bg-tertiary cursor-pointer p-2 border-2 border-danger hover:bg-danger hover:text-tertiary hover:font-bold">
+								delete
+							</button>
+						</div>
+					</li>
 				))}
 			</ul>
 		</div>
