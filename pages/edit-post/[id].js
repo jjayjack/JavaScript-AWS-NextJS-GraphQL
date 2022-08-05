622081b7a4c554874689049a78f1009ebfaebc34
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-08-05 11:42:26 -0500

    Fetching post based on ID

--- pages/edit-post/[id].js
+++ pages/edit-post/[id].js
@@ -1,4 +1,4 @@
-import { useState, useRef, React } from "react";
+import { useState, useRef, useEffect } from "react";
 import { API, Auth } from "aws-amplify";
 import { useRouter } from "next/router";
 import "easymde/dist/easymde.min.css";
@@ -9,3 +9,27 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
 import { updatePost } from "../src/graphql/mutations";
 import { getPost } from "../../src/graphql/queries";
 import { v4 as uuid } from "uuid";
+
+function EditPost() {
+	const [post, setPost] = useState(null);
+	const router = useRouter();
+	const { id } = router.query;
+
+	useEffect(() => {
+		fetchPost();
+
+		async function fetchPost() {
+			if (!id) return;
+
+			const postData = await API.graphql({
+				query: getPost,
+				variables: { id }
+			});
+			setPost(postData.data.getPost);
+		}
+	}, [id]);
+
+	return <div></div>;
+}
+
+export default EditPost;
